63ff0ae3dbdad2af62f21cf84c7e1888e110b422
Michelle Tabor <michelle@brdigesstrategies.com>
2022-04-27 12:04:20 -0500

    refined to add link

--- sendToSlack.js
+++ sendToSlack.js
@@ -3,20 +3,50 @@ const axios = require('axios')
 exports.main = (event, callback) => {
 
   const dealName = event.inputFields['dealname'];
+  const portalID = PORTAL_ID
+  const dealID = event.inputFields['hs_object_id'];
 
   const headers = {
     'Content-Type': 'application/json'
   };
 
-  const slackWebhookURL = "";
+  const slackWebhookURL = SLACK_URL;
   const requestBody = {
-	"text":`${dealName} is closed!`
+      "blocks": [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain_text",
+            "text": "New Deal Closed",
+            "emoji": true
+          }
+        },
+        {
+          "type": "section",
+          "fields": [
+            {
+              "type": "mrkdwn",
+              "text": `*Name:*\n ${dealName}`
+            }
+          ]
+        },
+        {
+          "type": "section",
+            "fields": [
+               {
+                "type": "mrkdwn",
+                "text": `<https://app.hubspot.com/contacts/${portalID}/deal/${dealID}/|View in HubSpot>`,
+               }
+            ]
+        }
+      ]
     }
+ 
 
 
   axios
     .post(
-      'slackWebhookURL',
+      slackWebhookURL,
       requestBody,
       { headers }
     )
